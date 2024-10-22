596fa254de6298386dc6fb48f7811245180e195c
DDorka02 <dobszaydorka@gmail.com>
2024-10-22 15:52:54 +0200

    jav

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,32 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\USerController;
+use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+//bárki által elérhető
+Route::post('/register',[RegisteredUserController::class, 'store']);
+Route::post('/login',[AuthenticatedSessionController::class, 'store']);
+Route::apiResource('users', USerController::class);
+Route::patch('update-password/{id}'), [USerController::class,"updatePassword"];
+
+//autentikált utvonal
+Route::middleware(['auth:sanctum'])
+->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 });
+
+//admin
+
+Route::middleware(['auth:sanctum',Admin::class])
+->group(function () {
+    Route::get('/admin/users', [USerController::class, 'index']);
+});
+
+
